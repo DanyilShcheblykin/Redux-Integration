e9e51718f3f0a379610e23e9fe45240735792a5c
DanyilShcheblykib <danyil.shch@gmail.com>
2022-08-11 16:40:29 +0300

    Add midalware logic

--- src/components/repoCard.tsx
+++ src/components/repoCard.tsx
@@ -26,7 +26,7 @@ const RepoCard = ({ repo }: { repo: IRepo }) => {
   };
 
   return (
-    <div className="border py-3 px-5 cursor-pointer mb-2 active:bg-green-200 hover:bg-gray-400 transition-all">
+    <div className="border  py-3 px-5 cursor-pointer mb-2 active:border-red-400 hover:bg-gray-400 transition-all">
       <a href={repo.html_url} target="_blank">
         <h2 className="text-lg font-bold">{repo.full_name}</h2>
         <p className="text-sm">
@@ -39,14 +39,14 @@ const RepoCard = ({ repo }: { repo: IRepo }) => {
       {isFev ? (
         <button
           onClick={removeFromFavorite}
-          className="py-2 px-4 bg-red-400 rounded hover:bg-red-500 active:bg-red-800 "
+          className="py-2 px-4  bg-red-400 rounded hover:bg-red-500 active:bg-red-800 "
         >
           Remove
         </button>
       ) : (
         <button
           onClick={addToFavorite}
-          className="mr-6 py-2 px-4 bg-yellow-400 rounded hover:bg-yellow-500 active:bg-green-300 "
+          className="mr-6 py-2 px-4  bg-yellow-400 rounded hover:bg-yellow-500 active:bg-green-300 "
         >
           Add
         </button>
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,26 @@ import { gitHubApi } from './github/gitHub.api';
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
+function exampleMiddleware(storeAPI: any) {
+    return function wrapDispatch(next: any) {
+      return function handleAction(action: any) {
+        if (action.payload !== undefined) {
+          if (action.payload.length) {
+            action.payload = action.payload.slice(0 , -1);
+          }
+        }
+  
+        return next(action);
+      };
+    };
+  }
+
 export const store = configureStore({
     reducer:{
         [gitHubApi.reducerPath]:gitHubApi.reducer , 
         github: gitHubreducer
     },
-    middleware: getDefaultMiddleware=>getDefaultMiddleware().concat(gitHubApi.middleware)
+    middleware: getDefaultMiddleware=>getDefaultMiddleware().concat(gitHubApi.middleware , exampleMiddleware)
 })
 
 console.log(gitHubApi.middleware)
